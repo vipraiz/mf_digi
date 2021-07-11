43ef6bcf7ffbd2cea30a2257f022430a6fe3de71
vipraiz <vipraizhas@gmail.com>
2021-07-11 20:54:16 +0800

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button type="button" class="slick-arrow slick-prev"><img src="img/prev-arrow.svg" alt="prev-arrow" /></button>',
+    nextArrow:
+      '<button type="button" class="slick-arrow slick-next"><img src="img/next-arrow.svg" alt="next-arrow" /></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
